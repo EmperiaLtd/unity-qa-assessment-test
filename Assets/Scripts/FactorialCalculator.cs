befde8c105ebac653dfa1c52cd1f36f42928a339
hiimzimmy <holcmannsimonas@gmail.com>
2022-04-05 10:45:40 +0300

    fix(factorial-calculator): make class static

--- Assets/Scripts/FactorialCalculator.cs
+++ Assets/Scripts/FactorialCalculator.cs
@@ -1,6 +1,6 @@
 using System.Linq;
 
-public sealed class FactorialCalculator
+public static class FactorialCalculator
 {
-    public float CalculateFactorial(int number) => Enumerable.Range(1, number).Aggregate(1, (p, item) => p * item);
+    public static float CalculateFactorial(int number) => Enumerable.Range(1, number).Aggregate(1, (p, item) => p * item);
 }
